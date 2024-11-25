723c55e2de6efcc36fae480a9b155b9ffb286b9b
Eduard <parhomenkoeduard75@gmail.com>
2024-11-25 15:23:20 +0200

    fix: fixed merged styles issues, added proper styles to container and main, changed colors to vars and added var colors for dark theme.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import './js/faq';
+import './js/header-modal';
+import './js/my-projects';
+import './js/work-together';
 // REVIEWS SECTION
-import { reviewsSection, fetchReviews } from './reviews';
+import { reviewsSection, fetchReviews } from './js/reviews';
 reviewsSection();
 fetchReviews();
