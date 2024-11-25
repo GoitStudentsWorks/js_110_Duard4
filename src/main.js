faf428bff9badcec758d621214da81f64968f096
Eduard <parhomenkoeduard75@gmail.com>
2024-11-25 17:32:17 +0200

    feat: add dark theme, fix img paths, delete reduntant css file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './js/faq';
 import './js/header-modal';
 import './js/my-projects';
 import './js/work-together';
+import './js/theme-change.js';
 // REVIEWS SECTION
 import { reviewsSection, fetchReviews } from './js/reviews';
 reviewsSection();
